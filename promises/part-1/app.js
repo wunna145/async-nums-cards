827b97ff2fc77717688b19b9c82fc7beecbeabb4
Zaw Wunna <zawwunna@Zaws-Air.lan>
2023-09-02 15:06:57 -0700

    added promises exercises

---
+++ promises/part-1/app.js
@@ -0,0 +1,24 @@
+let favNumber = 4;
+let baseURL = "http://numbersapi.com";
+
+//1.
+$.getJSON(`${baseURL}/${favNumber}?json`)
+    .then(data => {
+       console.log(data); 
+    });
+
+//2.
+let favNumbers = [7, 14, 24];
+$.getJSON(`${baseURL}/${favNumbers}?json`)
+    .then( data => {
+        console.log(data);
+    });
+    
+//3.
+Promise.all(
+    Array.from({length: 4}, () => {
+        return $.getJSON(`${baseURL}/${favNumber}?json`);
+    })
+).then(facts => {
+    facts.forEach(data => $("body").append(`<p>${data.text}</p>`))
+});
